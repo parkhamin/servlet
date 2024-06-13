3cea542d1ab5742bf507b2d92c34fa03426531a3
박하민 <parkhamint@naver.com>
2024-06-13 19:54:40 +0900

    유연한 컨트롤러2 - v5

--- src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -4,7 +4,11 @@ import hello.servlet.web.frontcontroller.MyView;
 import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,22 +22,26 @@ import java.util.List;
 import java.util.Map;
 
 @WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/ v5/*")
-     public class FrontControllerServletV5 extends HttpServlet {
-     private final Map<String, Object> handlerMappingMap = new HashMap<>();
-     private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
-     public FrontControllerServletV5() {
-     initHandlerMappingMap();
-     initHandlerAdapters();
+    public class FrontControllerServletV5 extends HttpServlet {
+    private final Map<String, Object> handlerMappingMap = new HashMap<>();
+    private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+    public FrontControllerServletV5() {initHandlerMappingMap();initHandlerAdapters();
  }
 
  private void initHandlerMappingMap() {
      handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
      handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
      handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+     // v4 추가
+     handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+     handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+     handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
  }
 
  private void initHandlerAdapters() {
-    handlerAdapters.add(new ControllerV3HandlerAdapter());
+     handlerAdapters.add(new ControllerV3HandlerAdapter());
+     handlerAdapters.add(new ControllerV4HandlerAdapter());
  }
 
  @Override
